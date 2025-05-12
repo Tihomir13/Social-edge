f459e4e8c439126f775cc7e594e5ea2be6b83868
Tihomir13 <tihomir13n2002@abv.bg>
2025-05-12 15:40:59 +0300

    update fe api

--- src/app/shared/constants/api.ts
+++ src/app/shared/constants/api.ts
@@ -1,4 +1,4 @@
 // export const api = 'http://localhost:3000';
-export const feApi = 'http://localhost:4200';
+export const feApi = 'https://social-edge.netlify.app';
 // export const api = 'http://192.168.30.163:3000';
 export const api = 'https://social-edge-server-production.up.railway.app';
