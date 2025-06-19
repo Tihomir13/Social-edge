6d6a41294edcf44c64b328657052142de3fc2edf
Tihomir13 <tihomir13n2002@abv.bg>
2025-06-19 14:58:56 +0300

    fixing bug with post modal and removing validation for files, adding filetype constant

--- src/app/shared/constants/settings.ts
+++ src/app/shared/constants/settings.ts
@@ -2,3 +2,10 @@ export const maxImageSize = 10; // MB
 export const postsLimitPerFetch = 10;
 export const commentsLimitPerFetch = 10;
 export const chatLimitMessagesPerFetch = 30;
+
+export const validImageFileTypes = ['image/png',
+    'image/jpeg',
+    'image/gif',
+    'image/webp',
+    'image/svg+xml',
+    'image/avif']
